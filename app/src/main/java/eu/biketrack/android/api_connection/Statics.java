c3c1890f0aea9fd40bb561075fe1c37d7ded8fbb
AdrienS <didishrek80@gmail.com>
2016-09-17 15:42:21 +0100

    Change graddle applicationId
    update appcompat
    create packages
    create statics

---
+++ app/src/main/java/eu/biketrack/android/api_connection/Statics.java
@@ -0,0 +1,9 @@
+package eu.biketrack.android.api_connection;
+
+/**
+ * Created by adrienschricke on 17/09/2016.
+ */
+public class Statics {
+    public static final String ROOT_API = "https://biketrack-api.herokuapp.com/api/";
+
+}
