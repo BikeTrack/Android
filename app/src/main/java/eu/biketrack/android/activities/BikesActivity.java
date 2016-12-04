510347178afcbd1b4abfd1ee332cc498d1e78e17
AdrienS <didishrek80@gmail.com>
2016-12-04 16:01:23 +0000

    Add listview for bikes

--- app/src/main/java/eu/biketrack/android/activities/BikesActivity.java
+++ app/src/main/java/eu/biketrack/android/activities/BikesActivity.java
@@ -6,11 +6,32 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import eu.biketrack.android.R;
 
 public class BikesActivity extends AppCompatActivity {
 
+    ListView list;
+    String[] itemname ={
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War"
+    };
+    String[] itemnameb ={
+            "A",
+            "b",
+            "c",
+            "d",
+            "e"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +39,36 @@ public class BikesActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, null);
+        CustomListAdapter adapterb=new CustomListAdapter(this, itemnameb, null);
+        list=(ListView)findViewById(R.id.listView_bikes);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                //list.setAdapter(adapterb);
             }
         });
     }
 
 }
+
