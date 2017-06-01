081f305e795e1e1d05dbbd0c3ecd40371e4f02bd
AdrienS <didishrek80@gmail.com>
2017-06-01 16:41:35 +0200

    Add map on Bike view with pointer

--- app/src/main/java/eu/biketrack/android/fragments/BikeFragment.java
+++ app/src/main/java/eu/biketrack/android/fragments/BikeFragment.java
@@ -1,14 +1,27 @@
 package eu.biketrack.android.fragments;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,7 +34,7 @@ import eu.biketrack.android.models.data_reception.Bike;
 import io.reactivex.disposables.CompositeDisposable;
 
 
-public class BikeFragment extends Fragment {
+public class BikeFragment extends Fragment implements OnMapReadyCallback {
     private static String TAG = "BIKETRACK - SendBikeInfo";
     private Unbinder unbinder;
     private AuthenticateReception auth;
@@ -33,6 +46,7 @@ public class BikeFragment extends Fragment {
     @BindView(R.id.bike_colour_tv) TextView _colour;
     @BindView(R.id.bike_brand_tv) TextView _brand;
     @BindView(R.id.bike_trackerid_tv) TextView _trackerid;
+    @BindView(R.id.map) MapView mapView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,6 +70,9 @@ public class BikeFragment extends Fragment {
         _colour.setText(bike.getColor());
         _brand.setText(bike.getBrand());
         _trackerid.setText(bike.getTracker());
+
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
         return layout;
     }
 
@@ -66,7 +83,46 @@ public class BikeFragment extends Fragment {
 
     @Override
     public void onDestroyView() {
+        mapView.onDestroy();
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        mapView.onLowMemory();
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions()
+                .position(new LatLng(50.639131, 3.049474)));
+        googleMap.getUiSettings().setMyLocationButtonEnabled(false);
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return ;
+        }
+        googleMap.setMyLocationEnabled(true);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        MapsInitializer.initialize(this.getActivity());
+        LatLngBounds.Builder builder = new LatLngBounds.Builder();
+        builder.include(new LatLng(50.639131, 3.049474));
+        LatLngBounds bounds = builder.build();
+        int padding = 0;
+        // Updates the location and zoom of the MapView
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding);
+        googleMap.moveCamera(cameraUpdate);
     }
 }
